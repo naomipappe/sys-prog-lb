6440449db4a5ccb0c408122401f9d0ef579dc0fd
Igor <nomad99.zverev@gmail.com>
2019-02-24 15:05:25 +0200

    initial draft, refactoring needed

--- DFA/src/main/java/com/naomipappe/DFA/NDFA.java
+++ DFA/src/main/java/com/naomipappe/DFA/NDFA.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+
 public class NDFA
 {   //all sets are made TreeSets cause we need fast search and fast elements retrieval O(log(n))
     /*transition map(table) is made this way because we have Nondeterministic finite automaton
@@ -22,55 +23,48 @@ public class NDFA
     private Set<Integer> NDFAfinalStates;
     private Integer DEVIL_STATE = -1;
     private Map<Integer,Map<Character,Set<Integer>>> NDFAtransitionMap;
-    private final StringBuilder ALLOWED_WORD;
+    private final StringBuilder ACCEPTED_WORD_BUILDER;
 
-    private void transitionMapLoader(String fileLine){
-        //transition goes like this (s, a, s')
-        String [] transition = fileLine.split("\\s+");
-        if(!NDFAtransitionMap.containsKey(Integer.parseInt(transition[0]))){
-            NDFAtransitionMap.put(Integer.parseInt(transition[0]), new TreeMap<>());
-        }
-        Map<Character,Set<Integer>> curEdges = NDFAtransitionMap.get(Integer.parseInt(transition[0]));
-        if(!curEdges.containsKey(transition[1].charAt(0))){
-            curEdges.put(transition[1].charAt(0), new TreeSet<>());
-        }
-        curEdges.get(transition[1].charAt(0)).add(Integer.parseInt(transition[2]));
-    }
-    private void getAcceptedWord(){
-        Set<Integer> used = new TreeSet<>();
-        DFS(used,initialState);
-        System.out.println(ALLOWED_WORD.toString());
-    }
 
-    private void DFS(Set<Integer> used,Integer state){
-        used.add(state);
-        for(Character key: NDFAtransitionMap.get(state).keySet() ){
-            ALLOWED_WORD.append(key);
-            for(Integer point: NDFAtransitionMap.get(state).get(key)){
-                if(NDFAfinalStates.contains(point)){
-                    return;
-                }
-                if (!used.contains(point)){
-                    used.add(point);
+    private String getAcceptedWord(){
+        DFS(initialState,new TreeSet<>());
+        return Stream.of(ACCEPTED_WORD_BUILDER.toString().split("\\|")).findAny().get();
+    }
+    private void DFS(Integer current, Set<Integer> marked) {
+        /*TODO implement DFS in such way that only one word is made, or as a stream
+        maybe something like return.any
+        */
+        if(NDFAfinalStates.contains(current)){
+            ACCEPTED_WORD_BUILDER.append('|');
+            return;
+        }
+        if(marked.contains(current)){
+            return;
+        }
+        if(!NDFAtransitionMap.keySet().contains(current)){
+            return;
+        }
+        marked.add(current);
+        for(Character key: NDFAtransitionMap.get(current).keySet()){
+            for(Integer state: NDFAtransitionMap.get(current).get(key)){
+                if(!marked.contains(state)){
+                ACCEPTED_WORD_BUILDER.append(key);
+                DFS(state, marked);
                 }
             }
         }
-
-
     }
 
+
     private boolean accept(String w2, String w1){
-        getAcceptedWord();
-        String acceptedString = w2 + ALLOWED_WORD.toString() + w1;
+        String acceptedString = w2 + getAcceptedWord() + w1;
         Set<Integer> currentStates = new TreeSet<>();
         currentStates.add(initialState);
         for(int i = 0; i<acceptedString.length(); ++i){
             currentStates = nextStates(currentStates,acceptedString.charAt(i));
         }
-        currentStates.forEach(s-> System.out.print(s+" "));
         return currentStates.stream().anyMatch(s->NDFAfinalStates.contains(s));
     }
-
     private Set<Integer> nextStates (Set<Integer> currStates, Character signal){
         Set<Integer> nextStates = new TreeSet<>();
         if(!NDFAAlphabet.contains(signal)){
@@ -86,13 +80,12 @@ public class NDFA
         NDFAStates = new TreeSet<>();
         NDFAfinalStates = new TreeSet<>();
         NDFAtransitionMap = new TreeMap<>();
-        ALLOWED_WORD = new StringBuilder();
+        ACCEPTED_WORD_BUILDER = new StringBuilder();
     }
     public NDFA (String file){
         this();
         this.initFromFile(file);
     }
-
     public boolean initFromFile(String file){
         LinkedList<String> lines;
 
@@ -155,14 +148,24 @@ public class NDFA
         NDFAtransitionMap.forEach((s,k)-> System.out.println(NDFAtransitionMap.get(s)));
         return true;
     }
-
+    private void transitionMapLoader(String fileLine){
+        //transition goes like this (s, a, s')
+        String [] transition = fileLine.split("\\s+");
+        if(!NDFAtransitionMap.containsKey(Integer.parseInt(transition[0]))){
+            NDFAtransitionMap.put(Integer.parseInt(transition[0]), new TreeMap<>());
+        }
+        Map<Character,Set<Integer>> curEdges = NDFAtransitionMap.get(Integer.parseInt(transition[0]));
+        if(!curEdges.containsKey(transition[1].charAt(0))){
+            curEdges.put(transition[1].charAt(0), new TreeSet<>());
+        }
+        curEdges.get(transition[1].charAt(0)).add(Integer.parseInt(transition[2]));
+    }
 
     public static void main( String[] args )
     {
         Scanner in = new Scanner(System.in);
         String filename = in.nextLine();
         NDFA automaton = new NDFA(filename);
-        System.out.println(automaton.accept("c","a"));
-
+        System.out.println(automaton.accept("",""));
     }
 }
